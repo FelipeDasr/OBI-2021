0895ab7bc08a4a2b6d01e68ac1e6388bd3e291ca
FelipeDasr <felipe.heiker07@gmail.com>
2022-06-07 17:56:38 -0300

    first structure

--- fase-1/baralho.cpp
+++ fase-1/baralho.cpp
@@ -1,10 +1,23 @@
-// URL do exercício: https://olimpiada.ic.unicamp.br/pratique/ps/2021/f1/baralho/
+// URL do exerc�cio: https://olimpiada.ic.unicamp.br/pratique/ps/2021/f1/baralho/
 
 #include <iostream>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
 int main(){
 
+    string baralho;
+    cin >> baralho;
+
+    /*for (int i = 0; i < baralho.size(); i+=3){
+        int inicio = baralho[i] - '0';
+        int fim = baralho[i+1] - '0';
+        char naipe = baralho[i+2];
+
+        cout << inicio << " " << fim << " " << naipe << endl;
+    }*/
+    
     return 0;
 }
