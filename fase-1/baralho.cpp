1123d60c17b6aa6aa5c44d2dde177680207f0791
FelipeDasr <felipe.heiker07@gmail.com>
2022-06-07 22:00:50 -0300

    Exercise solution (OOP)

--- fase-1/baralho.cpp
+++ fase-1/baralho.cpp
@@ -2,22 +2,98 @@
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
 #include <string>
 
 using namespace std;
 
-int main(){
+class Naipe {
+public:
+    vector<int> cartas;
+    string value = "";
+};
 
-    string baralho;
-    cin >> baralho;
+class Baralho {
+public:
+    Naipe C = Naipe();
+    Naipe E = Naipe();
+    Naipe U = Naipe();
+    Naipe P = Naipe();
 
-    /*for (int i = 0; i < baralho.size(); i+=3){
-        int inicio = baralho[i] - '0';
-        int fim = baralho[i+1] - '0';
-        char naipe = baralho[i+2];
+    Naipe *pegarNaipe(char letra) {
+        switch (letra) {
+        case 'C':
+            return &this->C;
+            break;
 
-        cout << inicio << " " << fim << " " << naipe << endl;
-    }*/
-    
+        case 'U':
+            return &this->U;
+            break;
+
+        case 'E':
+            return &this->E;
+            break;
+
+        case 'P':
+            return &this->P;
+            break;
+        }
+    }
+
+    void addCarta(int num, char nai) {
+
+        Naipe* naipe = this->pegarNaipe(nai);
+
+        if (naipe->value == "erro") {
+            return;
+        };
+
+        auto busca = find(
+            naipe->cartas.begin(),
+            naipe->cartas.end(),
+            num
+        );
+
+        if(busca == naipe->cartas.end()) {
+            naipe->cartas.push_back(num);
+        } 
+        else {
+            naipe->value = "erro";
+        }
+    }
+
+    auto mostrarResultado(Naipe* naipe) {
+        if (naipe->value == "erro"){
+            cout << "erro" << endl;
+        }
+        else {
+            cout << 13 - naipe->cartas.size() << endl;
+        }
+    }
+};
+
+int main() {
+    Baralho baralho = Baralho();
+
+    string baralhoStr;
+    cin >> baralhoStr;
+
+    for (int i = 0; i < baralhoStr.size(); i+=3) {
+
+        string numeroStr = "";
+        numeroStr += baralhoStr[i];
+        numeroStr += baralhoStr[i+1];
+
+        int numero = stoi(numeroStr);
+        char naipe = baralhoStr[i+2];
+
+        baralho.addCarta(numero, naipe);
+    }
+
+    baralho.mostrarResultado(&baralho.C);
+    baralho.mostrarResultado(&baralho.E);
+    baralho.mostrarResultado(&baralho.U);
+    baralho.mostrarResultado(&baralho.P);
+
     return 0;
 }
